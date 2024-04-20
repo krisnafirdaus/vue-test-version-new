5fbf5c3cd46e0311eea289ef01f5e0c8bdbf847c
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-04-20 14:54:14 +0700

    initial project

---
+++ src/components/Mapping.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello Mappirefng')
+const todos = ref([])
+const newTodo = ref('')
+
+const handleClick = () => {
+	msg.value = 'Hello Change'
+	console.log(msg.value)
+}
+
+const AddTodo = () => {
+	todos.value.push({
+		id: Date.now(),
+		text: 'New Todo'
+	})
+}
+
+const data = [
+	{
+		id: 1, 
+		name: 'John Doe',
+		age: 30,
+	},
+	{
+		id: 2,
+		name: 'Andi',
+		age: 20,
+	}
+]
+
+</script>
+
+<template>
+	<div class="mapping" @click="handleClick">Hello Mapping</div>
+	<ul>
+		<li v-for="item in data" :key="item.id">
+			{{ item.name }} - {{ item.age }}
+		</li>
+	</ul>
+</template>
+
+<style scoped>
+ .mapping {
+	color: red;
+ }
+</style>
